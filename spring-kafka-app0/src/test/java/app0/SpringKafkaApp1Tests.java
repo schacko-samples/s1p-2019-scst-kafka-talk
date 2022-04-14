ec9cb470bf097ca57907958c10754915e9658cb0
Soby Chacko <schacko@pivotal.io>
2022-04-13 20:14:56 -0400

    cleanup

--- spring-kafka-app0/src/test/java/app0/SpringKafkaApp1Tests.java
+++ spring-kafka-app0/src/test/java/app0/SpringKafkaApp1Tests.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.junit.jupiter.api.Test;
 
